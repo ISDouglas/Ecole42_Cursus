6cc9727565a24a16da533890635daa0b308c8d19
Lan Yang <layang@c2r4p3.42perpignan.fr>
2025-01-25 15:39:35 +0100

    add version for 125 score

--- 04_push_swap_125/srcs_bonus/checker_utils.c
+++ 04_push_swap_125/srcs_bonus/checker_utils.c
@@ -6,7 +6,7 @@
 /*   By: layang <layang@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 11:39:08 by layang            #+#    #+#             */
-/*   Updated: 2025/01/25 15:13:11 by layang           ###   ########.fr       */
+/*   Updated: 2025/01/25 15:36:35 by layang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,38 +71,38 @@ int	is_sorted_a(t_list	*stack)
 	}
 	free(input);
 	return (1);
-} */
+} rrr and rr missed */
 
-void	instruct_rr(char	*input, t_stack	*tab)
+static void	instruct_rr(char *input, t_stack *tab)
 {
-	if (ft_strncmp(input, "rr\n", 2) == 0)
+	if (ft_strncmp(input, "rr\n", 3) == 0)
 		rr(&tab->a, &tab->b);
-	else if (ft_strncmp(input, "rra\n", 3) == 0)
+	else if (ft_strncmp(input, "rra\n", 4) == 0)
 		rra(&tab->a);
-	else if (ft_strncmp(input, "rrb\n", 3) == 0)
+	else if (ft_strncmp(input, "rrb\n", 4) == 0)
 		rrb(&tab->b);
-	else if (ft_strncmp(input, "rrr\n", 3) == 0)
+	else if (ft_strncmp(input, "rrr\n", 4) == 0)
 		rrr(&tab->a, &tab->b);
 }
 
 int	instruct(char	*input, t_stack	*tab)
 {
-	if (ft_strncmp(input, "sa\n", 2) == 0)
+	if (ft_strncmp(input, "sa\n", 3) == 0)
 		sa(&tab->a);
-	else if (ft_strncmp(input, "sb\n", 2) == 0)
+	else if (ft_strncmp(input, "sb\n", 3) == 0)
 		sb(&tab->b);
-	else if (ft_strncmp(input, "ss\n", 2) == 0)
+	else if (ft_strncmp(input, "ss\n", 3) == 0)
 		ss(&tab->a, &tab->b);
-	else if (ft_strncmp(input, "pa\n", 2) == 0)
+	else if (ft_strncmp(input, "pa\n", 3) == 0)
 		pa(&tab->a, &tab->b);
-	else if (ft_strncmp(input, "pb\n", 2) == 0)
+	else if (ft_strncmp(input, "pb\n", 3) == 0)
 		pb(&tab->a, &tab->b);
-	else if (ft_strncmp(input, "ra\n", 2) == 0)
+	else if (ft_strncmp(input, "ra\n", 3) == 0)
 		ra(&tab->a);
-	else if (ft_strncmp(input, "rb\n", 2) == 0)
+	else if (ft_strncmp(input, "rb\n", 3) == 0)
 		rb(&tab->b);
-	else if (!ft_strncmp(input, "rr\n", 2) || !ft_strncmp(input, "rra\n", 3)
-		|| !ft_strncmp(input, "rrb\n", 3) || !ft_strncmp(input, "rrr\n", 3))
+	else if (!ft_strncmp(input, "rr\n", 3) || !ft_strncmp(input, "rra\n", 4)
+		|| !ft_strncmp(input, "rrb\n", 4) || !ft_strncmp(input, "rrr\n", 4))
 		instruct_rr(input, tab);
 	else
 	{

f42fbe6c77c592b60b50b35713515c0dea843a9c
lanYangweb <silvanaxiong@qq.com>
2025-02-02 15:49:20 +0100

    add pipex

---
+++ pipex/pipex_me/note_code.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   note_code.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: layang <layang@student.42perpignan.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 12:15:14 by layang            #+#    #+#             */
+/*   Updated: 2025/02/02 12:15:20 by layang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void	exec_cmd(char *cmd, char **envp)
+{
+	char *args[] = {"/bin/sh", "-c", cmd, NULL};
+	execve("/bin/sh", args, envp);
+	perror("execve");
+	exit(1);
+}
+
+void	child_process(int infile, int pipefd[2], char *cmd, char **envp)
+{
+	dup2(infile, STDIN_FILENO);   // 标准输入重定向到 infile
+	dup2(pipefd[1], STDOUT_FILENO); // 标准输出重定向到管道写端
+	close(pipefd[0]); // 关闭管道读端
+	exec_cmd(cmd, envp);
+}
+
+void	parent_process(int outfile, int pipefd[2], char *cmd, char **envp)
+{
+	dup2(pipefd[0], STDIN_FILENO); // 标准输入重定向到管道读端
+	dup2(outfile, STDOUT_FILENO); // 标准输出重定向到 outfile
+	close(pipefd[1]); // 关闭管道写端
+	exec_cmd(cmd, envp);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	if (argc != 5)
+	{
+		fprintf(stderr, "Usage: %s infile cmd1 cmd2 outfile\n", argv[0]);
+		return (1);
+	}
+
+	int infile = open(argv[1], O_RDONLY);
+	if (infile < 0)
+	{
+		perror("open infile");
+		return (1);
+	}
+
+	int outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (outfile < 0)
+	{
+		perror("open outfile");
+		close(infile);
+		return (1);
+	}
+
+	int pipefd[2];
+	if (pipe(pipefd) == -1)
+	{
+		perror("pipe");
+		close(infile);
+		close(outfile);
+		return (1);
+	}
+
+	pid_t pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		close(infile);
+		close(outfile);
+		return (1);
+	}
+
+	if (pid == 0) // 子进程
+	{
+		child_process(infile, pipefd, argv[2], envp);
+	}
+	else // 父进程
+	{
+		wait(NULL); // 等待子进程完成
+		parent_process(outfile, pipefd, argv[3], envp);
+	}
+
+	close(infile);
+	close(outfile);
+	return (0);
+}
---
+++ pipex/pipex_me/pipe.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: layang <layang@student.42perpignan.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 11:21:05 by layang            #+#    #+#             */
+/*   Updated: 2025/02/02 11:41:57 by layang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int a[2], b[2];
+    pid_t pid;
+
+    pipe(a); // 父进程到子进程的管道
+    pipe(b); // 子进程到父进程的管道
+
+    pid = fork();
+
+    if (pid == 0) // 子进程
+    {
+        char buffer[128];
+
+        close(a[1]); // 关闭写入端
+        close(b[0]); // 关闭读取端
+
+        read(a[0], buffer, sizeof(buffer)); // 从父进程读取
+        printf("Child received: %s\n", buffer);
+
+        const char *reply = "Hello, parent!";
+        write(b[1], reply, strlen(reply) + 1); // 写回父进程
+
+        close(a[0]);
+        close(b[1]);
+    }
+    else // 父进程
+    {
+        char buffer[128];
+
+        close(a[0]); // 关闭读取端
+        close(b[1]); // 关闭写入端
+
+        const char *message = "Hello, child!";
+        write(a[1], message, strlen(message) + 1); // 写入子进程
+
+        read(b[0], buffer, sizeof(buffer)); // 从子进程读取
+        printf("Parent received: %s\n", buffer);
+
+        close(a[1]);
+        close(b[0]);
+    }
+
+    return (0);
+}
+/*
+$ gcc -Wall -Wextra -Werror -o pipe pipe.c
+$ ./pipe
+Child received: Hello, child!
+Parent received: Hello, parent!
+
+*/

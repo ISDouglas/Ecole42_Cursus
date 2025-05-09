2ea404dfd0f703caf9e3779174b01a01604b8c44
Lan Yang <layang@c2r4p3.42perpignan.fr>
2025-05-09 09:26:15 +0200

    push philo and its github

--- 08_philosophers/philo/main.c
+++ 08_philosophers/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: layang <layang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 13:54:32 by layang            #+#    #+#             */
-/*   Updated: 2025/04/17 16:13:46 by layang           ###   ########.fr       */
+/*   Updated: 2025/04/18 13:33:20 by layang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,33 +32,45 @@ long	ft_get_time(void)
 	return (tv.tv_sec * 1000L + tv.tv_usec / 1000);
 }
 
-int	ft_init_table(t_schedule	*plan)
+int	ft_init_table(t_schedule	*plan, t_table	*tab)
 {
-	t_table		tab;
-
-	tab.forks = malloc(sizeof(pthread_mutex_t) * plan->nb_phi);
-	if (!tab.forks)
+	tab->forks = malloc(sizeof(pthread_mutex_t) * plan->nb_phi);
+	if (!tab->forks)
 		return (1);
-	pthread_mutex_init(&tab.log, NULL);
-	tab.philos = malloc(sizeof(t_philo) * plan->nb_phi);
-	if (!tab.philos)
-		return (1);
-	tab.dead = 0;
-	tab.nb_eat = plan->nb_eat;
-	tab.start_time = ft_get_time();
+	if (pthread_mutex_init(&tab->log, NULL) != 0)
+		return (2);
+	tab->philos = malloc(sizeof(t_philo) * plan->nb_phi);
+	if (!tab->philos)
+		return (3);
+	tab->dead = 0;
+	tab->nb_eat = plan->nb_eat;
+	tab->start_time = ft_get_time();
 	return (0);
+}
+
+void	ft_free_philo(t_table	*tab, int sign)
+{
+	if (sign == 2 || sign == 3)
+	{
+		free(tab->forks);
+		if (sign == 3)
+			pthread_mutex_destroy(&tab->log);
+	}
 }
 
 int	main(int	ac, char	**av)
 {
 	t_schedule	plan;
+	t_table		tab;
+	int			init;
 
 	if (ac != 5 || ac != 6)
 		return(printf("nb_philo, t_die, t_eat, t_sleep, nb_eat(option)\n"), 0);
 	// ctr on not more than 200 philos and not less than 60ms.
     ft_init_schedule(&plan, av);
-	if (ft_init_table(&plan))
-		return (1);
-	
+	init = ft_init_table(&plan, &tab);
+	if (init && init != 1)
+		return (ft_free_philo(&tab, init), 1);
+	ft_start_philo();
 	return (0);
 }

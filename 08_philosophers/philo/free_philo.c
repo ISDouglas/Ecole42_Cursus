9c948664586e7f7cf36ba660d0559fd5adee4951
Lan Yang <layang@c2r4p3.42perpignan.fr>
2025-05-14 13:47:58 +0200

    add code in 08_

--- 08_philosophers/philo/free_philo.c
+++ 08_philosophers/philo/free_philo.c
@@ -6,7 +6,7 @@
 /*   By: layang <layang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 12:17:10 by layang            #+#    #+#             */
-/*   Updated: 2025/05/14 13:37:12 by layang           ###   ########.fr       */
+/*   Updated: 2025/05/13 12:18:17 by layang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,8 @@ int	wrong_input_check(t_table	*tab, char	*av5)
 		tab->nb_eat = -1;
 	if (tab->nb_eat == 0)
 		return (printf("number of eating is 0.\n"), -1);
-	if (tab->nb_phi <= 0)
-		return (printf("number of philo must be 1-250.\n"), -1);
-	if (tab->t_die < 0 || tab->t_eat < 0 || tab->t_sleep < 0)
+	if (tab->nb_phi < 0 || tab->t_die < 0 || tab->t_eat < 0
+			|| tab->t_sleep < 0)
 		return (printf("time must be positive.\n"), -1);
 	return (0);
 }
@@ -76,5 +75,4 @@ void	ft_free_philo(t_table	*tab, int sign)
 		free(tab->forks_locks);
 		tab->forks_locks = NULL;
 	}
-	free(tab);
 }

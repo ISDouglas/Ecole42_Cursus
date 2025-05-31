0d66d51e10a26b09e6380fa9a058de0665428e5a
ISDouglas <155378432+ISDouglas@users.noreply.github.com>
2025-05-31 18:06:38 +0200

    Update hook_events.c
    mod hook_events

--- 06_FDF/src/hook_events.c
+++ 06_FDF/src/hook_events.c
@@ -27,22 +27,6 @@ int	close_fdf(t_vars	*all)
 	return (0);
 }
 
-/* void	free_fdf(int flag, ...)
-{
-	va_list	a_list;
-	void	*ptr;
-
-	va_start(a_list, flag);
-	if (flag == -1)
-		get_next_line(-1, 0);
-	while ((ptr = va_arg(a_list, void *)) != NULL)
-	{
-		free(ptr);
-		ptr = NULL;
-	}
-	va_end(a_list);
-} */
-
 void	free_fdf(int flag, ...)
 {
 	va_list	a_list;
@@ -59,42 +43,6 @@ void	free_fdf(int flag, ...)
 	}
 	va_end(a_list);
 }
-
-/* t_win_list				*new_win
-t_img	*img
-t_xvar	*xvar
-free(all->map), free(line), get_next_line(-1, 0) */
-
-/* typedef enum e_type
-{
-    TYPE_T_MAP,
-    TYPE_INT,
-    TYPE_STRING,
-    // 添加更多类型
-} t_type;
-
-void free_fdf(int flag, ...)
-{
-    va_list args;
-    void *ptr;
-    t_type type;
-
-    va_start(args, flag);
-    while ((ptr = va_arg(args, void *)) != NULL)
-    {
-        type = va_arg(args, t_type);  // 获取类型标识符
-        switch (type)
-        {
-            case TYPE_T_MAP:
-                free_map((t_map *)ptr);
-                break;
-            // 其他类型的处理
-            default:
-                break;
-        }
-    }
-    va_end(args);
-} */
 
 static int	key_hooks_esc(int keycode, t_vars	*all)
 {
